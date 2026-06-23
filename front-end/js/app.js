41c92847616dd0f66b2a56301e666e127f09ebde
dario204 <dario.rod551@gmail.com>
2026-06-23 15:32:33 -0300

    actualizado

--- front-end/js/app.js
+++ front-end/js/app.js
@@ -197,7 +197,7 @@ function renderTable() {
       <td><span class="badge ${saldoCls}">${e.saldo_disponible}d</span></td>
       <td>
         <button class="btn-delete-row" title="Eliminar empleado"
-          onclick="event.stopPropagation(); eliminarEmpleado(${e.id}, '${e.nombre_apellido.replace(/'/g,"\\'")}')">
+          onclick="event.stopPropagation(); eliminarEmpleado(${e.id}, '${(e.nombre_apellido || '').replace(/'/g,"\\'")}')">          
           <svg viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2.5"><polyline points="3 6 5 6 21 6"/><path d="M19 6l-1 14H6L5 6"/><path d="M10 11v6M14 11v6"/><path d="M9 6V4h6v2"/></svg>
         </button>
       </td>
